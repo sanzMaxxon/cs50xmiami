b7861707e5ba1cdacf188e7d63b909275cf6d0fa
sanzMaxxon <greg@sanzmaxxon.com>
2015-09-04 19:20:23 -0400

    Finished pset 6

--- psets/6.js
+++ psets/6.js
@@ -14,4 +14,12 @@ Look up the JavaScript string reference to find methods which may be useful!
 
 function mixUp(a, b) {
   // write your solution here...
+  var pre1 = a.substring(0, 2);
+  var post1 = a.substring(2);
+  var pre2 = b.substring(0, 2);
+  var post2 = b.substring(2);
+  a = pre2 + post1;
+  b = pre1 + post2;
+  console.log(a + ' ' + b);
 }
+mixUp ('ferry man', 'benton stop');
