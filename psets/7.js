4478e1f5ee88550e0c563aaa573b022ec140a421
sanzMaxxon <greg@sanzmaxxon.com>
2015-09-05 01:09:53 -0400

    Finished pset 7

--- psets/7.js
+++ psets/7.js
@@ -13,4 +13,15 @@ fixStart('babble'): 'ba**le'
 
 function fixStart(s) {
   // write your solution here...
+  var newWord = '';
+  for(var i = 0; i < s.length; i++) {
+    if(i > 0 && s[i] == s[0]) {
+      newWord = newWord + ('*');
+    }
+    else {
+      newWord = newWord + s[i];
+    }
+  }
+  console.log(newWord);
 }
+fixStart('brooklyn bridge babble');
