b6ad52e3de902fe93572e751cadf28eda554234b
Greg Sanchez <greg@sanzmaxxon.com>
2015-09-07 20:50:45 -0400

    Finished pset 20 (Revised -- Not sure I understood the instructions)

--- psets/20.js
+++ psets/20.js
@@ -10,12 +10,32 @@ Create an array to hold your top choices (colors, presidents, whatever).
 
 // write your solution here...
 var myArray = {
-  'color': 'blue',
-  'president': 'Bill Clinton',
-  'city': 'San Francisco',
-  'view': 'Lake Tahoe on a calm, clear day',
-  'whatever': 'Atlantic Ocean'
+  'color': ['blue', 'aqua', 'red', 'orange', 'purple'],
+  'president': ['Bill Clinton', 'Abraham Lincoln', 'Ronald Reagan', 'Franklin D Rosevelt'],
+  'city': ['San Francisco', 'Brussels', 'LA', 'London'],
+  'view': ['Lake Tahoe on a calm, clear day', 'a sunset on the west coast of PR', 'sunrise on the cliffs of Malibu'],
+  'whatever': ['Atlantic Ocean', 'Niagara Falls', 'Iguazu Falls', 'phosphorescent bay in Lajas PR', 'Mount Everest']
+}
+var suffixer = function(number){
+  switch (number) {
+    case 1:
+      return '1st';
+      break;
+    case 2:
+      return '2nd';
+      break;
+    case 3:
+      return '3rd';
+      break;
+    default:
+      return number + 'th';
+  }
 }
 for (var key in myArray){
-  console.log('My #1 choice of ' + key + ' is ' + myArray[key] + '!');
+  var i = 1;
+  for (var item in myArray[key]){
+    console.log('My ' + suffixer(i) + ' choice of ' + key + ' is ' + myArray[key][item] + '!');
+    i++
+  }
+  console.log(' ');
 }
